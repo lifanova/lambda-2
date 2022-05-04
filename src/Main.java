27fe43ed1eba2a2ea88ddde004ff725cc4b96128
vlifanova <vlifanova2@alfabank.ru>
2022-05-04 12:56:32 +0300

    Worker

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,9 @@
 public class Main {
     public static void main(String[] args) {
         OnTaskDoneListener listener = System.out::println;
+        OnTaskErrorListener listenerError = System.out::println;
 
-        Worker worker = new Worker(listener);
+        Worker worker = new Worker(listener, listenerError);
         worker.start();
     }
 }
